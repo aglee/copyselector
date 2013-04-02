d5dd4eae0ec4c84c0ee3537d2e9b4c1b780526e2
Andy Lee <aglee@mac.com>
2013-04-02 12:23:46 -0400

    Initial Commit

---
+++ CopySelector/CSAppDelegate.m
@@ -0,0 +1,23 @@
+//
+//  CSAppDelegate.m
+//  CopySelector
+//
+//  Created by Andy Lee on 4/2/13.
+//  Copyright (c) 2013 Andy Lee. All rights reserved.
+//
+
+#import "CSAppDelegate.h"
+
+@implementation CSAppDelegate
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ CopySelector/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  CopySelector
+//
+//  Created by Andy Lee on 4/2/13.
+//  Copyright (c) 2013 Andy Lee. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
